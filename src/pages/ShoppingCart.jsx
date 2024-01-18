ce70abc56207a8eb1080d1270051cf5177f8688b
ArnT81 <anders_soderberg@hotmail.com>
2024-01-18 11:49:55 +0100

    fixed react key warnings

--- src/pages/ShoppingCart.jsx
+++ src/pages/ShoppingCart.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from "styled-components";
 import { BsTrash3 } from "react-icons/bs";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const ShoppingList = styled.div`
   width: 700px;
@@ -51,58 +51,61 @@ const ShoppingList = styled.div`
   
 `;
 
-const ShoppingCart = ({shopping, setShopping}) => {
+const ShoppingCart = ({ shopping, setShopping }) => {
 
-        const changeAmount = (change, id) => {
-            let newArray = [...shopping];
-            let objectToChange = shopping.find(product => product.id === id);
-            if(!(objectToChange.amount === 1 && change === "decrease"))
-            {
-                objectToChange.amount += ( change === "decrease" ) ? -1 : 1;
-                setShopping(newArray);
-            }
-        }
-
-    const trashItem = (id) => {
-        let newArray = shopping.filter(product => product.id !== id);
-        setShopping(newArray);
+  const changeAmount = (change, id) => {
+    let newArray = [...shopping];
+    let objectToChange = shopping.find(product => product.id === id);
+    if (!(objectToChange.amount === 1 && change === "decrease")) {
+      objectToChange.amount += (change === "decrease") ? -1 : 1;
+      setShopping(newArray);
     }
+  }
 
-    let itemsCost = 0;
-    let shippingCost = 0;
+  const trashItem = (id) => {
+    let newArray = shopping.filter(product => product.id !== id);
+    setShopping(newArray);
+  }
+
+  let itemsCost = 0;
+  let shippingCost = 0;
 
 
-    return (
-        <ShoppingList>
-            <h1>Products to buy</h1>
-            {shopping.map(product => {
-                itemsCost+= product.price * product.amount;
-                return (
-                    <Link to={`/products/${product.id}`}>
-                        <div key={product.id}>
-                            <img src={`/img/${product.id}.png`} alt=""/>
-                            <div>
-                                <p>{product.brand}</p>
-                                <p>{product.description}</p>
-                            </div>
-                            <BsTrash3 onClick={() => trashItem(product.id)}/>
-                            <div>
-                                <p onClick={() => changeAmount("decrease", product.id)}>-</p>
-                                <p onClick={() => changeAmount("increase", product.id)}>+</p>
-                                <p>{product.amount}</p>
-                            </div>
-                            <p>{(product.price * product.amount).toFixed(2)}</p>
-                        </div>
-                    </Link>
-                )
-            })}
-            <p>Items cost: {itemsCost.toFixed(2)}</p>
-            <p>Shipping cost: {
-                shippingCost = itemsCost > 500 || itemsCost === 0 ? 0 : 79
-            }</p>
-            <p>Total cost: {(itemsCost + shippingCost).toFixed(2)}</p>
-        </ShoppingList>
-    );
+  return (
+    <ShoppingList>
+      <h1>Products to buy</h1>
+      {shopping.map(product => {
+        itemsCost += product.price * product.amount;
+
+        return (
+          <Link
+            key={product.id}
+            to={`/products/${product.id}`}
+          >
+            <div key={product.id}>
+              <img src={`/img/${product.id}.png`} alt="" />
+              <div>
+                <p>{product.brand}</p>
+                <p>{product.description}</p>
+              </div>
+              <BsTrash3 onClick={() => trashItem(product.id)} />
+              <div>
+                <p onClick={() => changeAmount("decrease", product.id)}>-</p>
+                <p onClick={() => changeAmount("increase", product.id)}>+</p>
+                <p>{product.amount}</p>
+              </div>
+              <p>{(product.price * product.amount).toFixed(2)}</p>
+            </div>
+          </Link>
+        )
+      })}
+      <p>Items cost: {itemsCost.toFixed(2)}</p>
+      <p>Shipping cost: {
+        shippingCost = itemsCost > 500 || itemsCost === 0 ? 0 : 79
+      }</p>
+      <p>Total cost: {(itemsCost + shippingCost).toFixed(2)}</p>
+    </ShoppingList>
+  );
 };
 
 export default ShoppingCart;
