7bfb7520be7435fab64ee1562e77f70913c1ba52
tobiasengberg <tobias.engberg@exilic.se>
2024-02-20 11:28:17 +0100

    Add data for user page

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -23,36 +23,25 @@ const User = () => {
             <h1>User</h1>
             <TabView>
                 <TabPanel header="Orders">
-                    <p className="m-0">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                    </p>
+                    {userData.orders.map( (order, index) =>
+                        (
+                            <p key={index}>{order}</p>
+                        )
+                    )}
+
                 </TabPanel>
                 <TabPanel header="Account details">
-                    <p className="m-0">
-                    Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam,
-                    eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo
-                    enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui
-                    ratione voluptatem sequi nesciunt. Consectetur, adipisci velit, sed quia non numquam eius modi.
-                    </p>
+                    <p>{userData.account}</p>
                 </TabPanel>
                 <TabPanel header="Invoices">
-                    <p className="m-0">
-                    At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti
-                    quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in
-                    culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio.
-                    Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus.
-                    </p>
+                    {userData.invoices.map( (invoice, index) =>
+                        (
+                            <p key={index}>{invoice}</p>
+                        )
+                    )}
                 </TabPanel>
                 <TabPanel header="Payment options">
-                    <p className="m-0">
-                        At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti
-                        quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in
-                        culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio.
-                        Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus.
-                    </p>
+                    <p>{userData.paymentOptions}</p>
                 </TabPanel>
             </TabView>
             </Container>
